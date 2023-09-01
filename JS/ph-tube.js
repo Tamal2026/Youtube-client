2509ae4b136cf9eb7fa56269c481b91ded56c86a
Tamal1 <tamalkrishna2026@gmail.com>
2023-09-02 01:15:18 +0600

    Blog Site complete

--- JS/ph-tube.js
+++ JS/ph-tube.js
@@ -1,28 +1,42 @@
-const phTube = async(id)=>{
-    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+const phTube = async()=>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/1000`);
     const data = await res.json();
-   const phTubeData = data.data;
-   displayAllData(phTubeData);
-}
-
-const displayAllData = (phTubeData) =>{
-
+    console.log(data.data);
     const tubeContainer = document.getElementById('tube-container');
-    const div = document.createElement('div');
-    div.classList = `  card w-96 bg-base-100 shadow-xl  `;
-    div.innerHTML = `  <figure><img src="" alt="Shoes" /></figure>
-    <div class="card-body">
-      <h2 class="card-title">${phTubeData.title}</h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
-      </div>
-    </div>  `
-    tubeContainer.appendChild(div);
+    data.data.forEach(phTubeMain => {
+
+      const div = document.createElement('div');
+      div.classList = `  card w-96 bg-base-100 shadow-xl  `;
+      div.innerHTML = `  <figure><img src="${phTubeMain.thumbnail}" /></figure>
+      <div class="card-body">
+      <div class="flex">
+      <divs>
+          <img src="' ${phTubeMain.authors.profile_picture} '" alt="">
+        </divs>
+        <div>
+          <h1 class="font-bold">Building a Winning UX Strategy <br> Using the Kano Model</h1>
+        </div>
+        
+   </div>
+   <p>'${phTubeMain.authors[0]}'</p>
+         `
+      tubeContainer.appendChild(div);
+    })
+
+  }
 
 
+// Find the "Blog" button by its ID
+const blogBtn = document.getElementById('blog-btn');
 
-}
+// Add a click event listener to the "Blog" button
+blogBtn.addEventListener('click', () => {
+  // Replace the URL below with the actual URL of your blog
+  const blogUrl = 'http://127.0.0.1:5500/JS/blog.html';
+
+  // Open the blog URL in a new window
+  window.open(blogUrl, '_blank');
+});
 
 
 phTube();
