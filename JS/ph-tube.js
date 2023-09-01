de39016d3932e1c36a5df16358bf060727b37f3a
Tamal1 <tamalkrishna2026@gmail.com>
2023-09-02 02:18:04 +0600

    Verified Icon added

--- JS/ph-tube.js
+++ JS/ph-tube.js
@@ -7,15 +7,22 @@ const phTube = async(id)=>{
 
       const div = document.createElement('div');
       div.classList = `  card w-96 bg-base-100 shadow-xl  `;
-      div.innerHTML = `  <figure><img src="${phTubeMain.thumbnail}" /></figure>
+      div.innerHTML = ` <img src=" ${phTubeMain.thumbnail}" alt="" style=" height: 200px;">
       <div class="card-body">
       <div class="flex gap-4">
       <div>
-          <img src=" ${phTubeMain.authors[0].profile_picture}" alt="" style="width: 48px; height: 48px; border-radius: 50%;">
+          <img src=" ${phTubeMain.authors[0].profile_picture}" alt="" style="width: 40px; height: 40px; border-radius: 50%;">
         </div>
         <div>
           <h1 class="font-bold">${phTubeMain.title}</h1>
-          <p>${phTubeMain.authors[0].profile_name}</p>
+          <div class="flex items-center gap-2">
+          <div>
+              <p class ="mt-2">${phTubeMain.authors[0].profile_name}</p>
+          </div>
+          <div>
+          <img class="mt-2" src="${phTubeMain.authors[0].verified ? 'Group 3.png' : 'unverified-image.png'}" alt="">
+          </div>
+      </div>
         </div>
    
    </div>
