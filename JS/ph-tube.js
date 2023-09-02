d3c6e335c19190216494c9534debc9b5b3810e4e
Tamal1 <tamalkrishna2026@gmail.com>
2023-09-02 21:55:30 +0600

    Clicked Button complete

--- JS/ph-tube.js
+++ JS/ph-tube.js
@@ -1,58 +1,85 @@
-const phTube = async()=>{
-    const res = await fetch(`   https://openapi.programming-hero.com/api/videos/category/1000 `);
+const handleCategory = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/videos/categories");
     const data = await res.json();
-    console.log(data.data);
-    const tubeContainer = document.getElementById('tube-container');
-    data.data.forEach(phTubeMain => {
 
-      const div = document.createElement('div');
-      div.classList = `  card w-96 bg-base-100 shadow-xl relative `;
-      div.innerHTML = ` <img src=" ${phTubeMain.thumbnail}" alt="" style=" height: 200px;">
-      <div class="absolute bottom-0 right-0">
-        <p class="bg-orange-400 px-3 py-2">${phTubeMain.others.posted_date}</p>
-    </div>
-      <div class="card-body">
-      <div class="flex gap-4">
-      <div>
-          <img src=" ${phTubeMain.authors[0].profile_picture}" alt="" style="width: 40px; height: 40px; border-radius: 50%;">
-        </div>
-        <div>
-          <h1 class="font-bold">${phTubeMain.title}</h1>
-          <div class="flex items-center gap-2">
-          <div>
-              <p class ="mt-2">${phTubeMain.authors[0].profile_name}</p>
-          </div>
-          <div>
-          <img class="mt-2" src="${phTubeMain.authors[0].verified ? 'Group 3.png' : 'unverified-image.png'}" alt="">
-          
-          </div>
-      </div>
-        </div>
-   </div>
-   <p class ="mt-2 ml-14">${phTubeMain.others.views} views</p>
+  const btnContainer = document.getElementById('btn-container');
+  data.data.forEach((btn)=>{
+const div = document.createElement('div');
+div.innerHTML= `  
+<button id="all-btn" onclick = "handlerLoad('${btn.category_id}')" class=" py-1 px-5 rounded-lg bg-slate-300 font-light ">${btn.category}</button>
+
+`
+btnContainer.appendChild(div);
+
+  })};
+const handlerLoad = async(id)=>{
+  const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id} `);
+  const data = await res.json();
+  const cardContainer = document.getElementById('card-container');
+  cardContainer.innerHTML = "";
+
+  if (data.data.length === 0) {
+
+    const noDataContainer = document.createElement('div');
+    noDataContainer.classList = 'no-data-container ';
+
+    
+    const noDataImage = document.createElement('img');
+    noDataImage.src = 'icon.png';
+    noDataImage.alt = '';
 
    
-         `
-      tubeContainer.appendChild(div);
-    })
+    const noDataMessage = document.createElement('p');
+    noDataMessage.textContent = ' Opps !! Videos available.';
 
+    noDataContainer.appendChild(noDataImage);
+    noDataContainer.appendChild(noDataMessage);
+
+    cardContainer.appendChild(noDataContainer);
+  } else {
+    data.data.forEach((videos)=>{
+      const div = document.createElement('div');
+    div.classList = `  card w-96 bg-base-100 shadow-xl relative `;
+    div.innerHTML =` 
+     <img src=" ${videos.thumbnail}" alt="" style=" height: 200px;">
+      <div class="absolute bottom-0 right-0">
+        ${
+          videos.others.posted_date
+            ? `<p class="bg-orange-400 px-3 py-2">${videos.others.posted_date}</p>`: ''
+        }
+      </div>
+      <div class="card-body">
+        <div class="flex gap-4">
+          <div>
+            <img src=" ${videos.authors[0].profile_picture}" alt="" style="width: 40px; height: 40px; border-radius: 50%;">
+          </div>
+          <div>
+            <h1 class="font-bold">${videos.title}</h1>
+            <div class="flex items-center gap-2">
+              <div>
+                <p class="mt-2">${videos.authors[0].profile_name}</p>
+              </div>
+              <div>
+                <img class="mt-2" src="${
+                  videos.authors[0].verified ? 'Group 3.png' : 'unverified-image.png'
+                }" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+        <p class="mt-2 ml-14">${videos.others.views} views</p>
+      </div>
+    `;
+    cardContainer.appendChild(div);
+    
+    })
   }
 
-const blogBtn = document.getElementById('blog-btn');
 
 
-blogBtn.addEventListener('click', () => {
- 
-  const blogUrl = 'http://127.0.0.1:5500/JS/blog.html';
-  window.open(blogUrl,'_blank');
-});
 
 
-// const MusicHandler = async (categoryId)=>{
 
-//   const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
-//   const data = await res.json();
-//   console.log(data);
-// }
-
-phTube();
+}
+    handleCategory();
+    handlerLoad('1000');
